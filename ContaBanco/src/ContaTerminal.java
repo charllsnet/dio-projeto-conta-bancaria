c896c6e7c0c453cfdf432971ad44202136ad114d
charllsnet <141534914+charllsnet@users.noreply.github.com>
2024-05-16 23:44:15 -0300

    Atualização das informações a serem criados

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar scanner
+
+        //Exibir as mensagens para nosso usuário
+
+        //Obter pela scanner os valores digitados pelo terminal
+
+        //Exibir informações da conta criada 
     }
 }
